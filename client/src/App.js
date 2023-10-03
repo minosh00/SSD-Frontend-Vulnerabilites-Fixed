2ed9ac99268480b8a8b9617e7c89e6d80a4ab1f1
Minosh_Imantha <86770967+minosh00@users.noreply.github.com>
2023-10-03 14:23:08 +0530

    cimmit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-pascal-case */
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Landingscreen from "./Components/Landingscreen";
 import Login from "./Components/Auth/Login";
 import Register from "./Components/Auth/Register";
@@ -39,10 +39,11 @@ import AllUsers from "./Components/Auth/AllUsers";
 import "antd/dist/antd.css";
 import "./App.css";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
-
 function App() {
   const [, setUser] = useState("");
   const [, setPosts] = useState([]);
+  const [userRole, setUserRole] = useState("");
+  const [authenticated, setAuthenticated] = useState(false);
 
   useEffect(() => {
     axios
@@ -52,59 +53,73 @@ function App() {
   }, []);
 
   useEffect(() => {
-    setUser(localStorage.getItem("userRole"));
+    const token = localStorage.getItem("token");
+    const storedUserRole = localStorage.getItem("userRole");
+
+    if (token) {
+      setAuthenticated(true);
+      setUserRole(storedUserRole);
+    } else {
+      setAuthenticated(false);
+      setUserRole("");
+    }
   }, []);
+
+  const ProtectedRoute = ({ element, adminRequired }) => {
+    if (!authenticated) {
+      return <Navigate to="/login" />;
+    }
+
+    if (adminRequired && userRole !== "admin") {
+      return <Navigate to="/unauthorized" />;
+    }
+
+    return element;
+  };
 
   return (
     <Router>
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<Landingscreen />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/Profile" element={<Profile />} />
+    <NavBar />
+    <Routes>
+      <Route exact path="/" element={<Landingscreen />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/register" element={<Register />} />
+      <Route exact path="/Profile" element={<Profile />} />
+      <Route exact path="/dashboard" element={<Home />} />
+      <Route path="/AllEmployee" element={<ProtectedRoute adminRequired element={<AllEmployee />} />} />
+      <Route path="/addemployee" element={<ProtectedRoute adminRequired element={<AddEmployee />} />} />
+      <Route path="/updateEmployeeByID/:id" element={<ProtectedRoute adminRequired element={<UpdateEmployee />} />} />
+      <Route path="/AllSuppliers" element={<ProtectedRoute adminRequired element={<AllSuppliers />} />} />
+      <Route path="/addsupplier" element={<ProtectedRoute adminRequired element={<AddSupplier />} />} />
+      <Route path="/updateSupplierByID/:id" element={<ProtectedRoute adminRequired element={<UpdateSupplyer />} />} />
+      <Route path="/AllMenus" element={<ProtectedRoute adminRequired={false} element={<AllMenus />} />} />
+      <Route path="/updateMenuByID/:id" element={<ProtectedRoute adminRequired={false} element={<EditMenu />} />} />
+      <Route path="/addMenu" element={<ProtectedRoute adminRequired={false} element={<AddMenu />} />} />
+      <Route path="/AllOrders" element={<ProtectedRoute adminRequired={false} element={<AllOrders />} />} />
+      <Route path="/AddRoom" element={<ProtectedRoute adminRequired={false} element={<AddRoom />} />} />
+      <Route path="/mainroom" element={<ProtectedRoute adminRequired={false} element={<MainRoom />} />} />
+      <Route path="/allroom" element={<ProtectedRoute adminRequired={false} element={<AllRooms />} />} />
+      <Route path="/updateRoomsByID/:id" element={<ProtectedRoute adminRequired={false} element={<UpdateRooms />} />} />
+      <Route path="/updateRoomsByID1/:id/:fromdate/:todate" element={<ProtectedRoute adminRequired={false} element={<DisplayOneRoom />} />} />
+      <Route path="/payroom" element={<ProtectedRoute adminRequired={false} element={<Room_Payment />} />} />
+      <Route path="/allbookingsroom" element={<ProtectedRoute adminRequired={false} element={<AllBookings />} />} />
+      <Route path="/cancelbook/:id" element={<ProtectedRoute adminRequired={false} element={<CancelBooking />} />} />
+      <Route path="/updateRoomsByIDcus/:id/:fromdate/:todate" element={<ProtectedRoute adminRequired={false} element={<Booking />} />} />
+      <Route path="/comments-section" element={<ProtectedRoute adminRequired={false} element={<CommentsSection />} />} />
+      <Route path="//comments-section/create/:roomID" element={<ProtectedRoute adminRequired={false} element={<AddComment />} />} />
+      <Route path="/comments-section/edit/:roomID/:id" element={<ProtectedRoute adminRequired={false} element={<EditComment />} />} />
+      <Route path="/allusers" element={<ProtectedRoute adminRequired={false} element={<AllUsers />} />} />
+      <Route path="/cusroom" element={<CusRoom />} />
+      <Route path="/Displaymenus" element={<Test />} />
+      <Route path="/AllMenus" element={<AllMenus />} />
+      <Route path="/payroom" element={<Room_Payment />} />
+      <Route path="/allbookingsroom" element={<AllBookings />} />
+      <Route path="/cancelbook/:id" element={<CancelBooking />} />
+      <Route path="/updateRoomsByIDcus/:id/:fromdate/:todate" element={<Booking />} />
 
-        <Route path="/AllEmployee" element={<AllEmployee />} />
-        <Route path="/addemployee" element={<AddEmployee />} />
-        <Route path="/updateEmployeeByID/:id" element={<UpdateEmployee />} />
-        <Route path="/AllSuppliers" element={<AllSuppliers />} />
-        <Route path="/addsupplier" element={<AddSupplier />} />
-        <Route path="/updateSupplierByID/:id" element={<UpdateSupplyer />} />
 
-        <Route exact path="/dashboard" element={<Home />} />
-        <Route path="/AllMenus" element={<AllMenus />} />
-        <Route path="/updateMenuByID/:id" element={<EditMenu />} />
-        <Route path="/addMenu" element={<AddMenu />} />
 
-        <Route path="/AllOrders" element={<AllOrders />} />
-        <Route path="/Displaymenus" element={<Test />} />
 
-        <Route path="/AddRoom" element={<AddRoom />} />
-        <Route path="/mainroom" element={<MainRoom />} />
-        <Route path="/allroom" element={<AllRooms />} />
-        <Route path="/updateRoomsByID/:id" element={<UpdateRooms />} />
-        <Route
-          path="/updateRoomsByID1/:id/:fromdate/:todate"
-          element={<DisplayOneRoom />}
-        />
-        <Route path="/cusroom" element={<CusRoom />} />
-        <Route path="/payroom" element={<Room_Payment />} />
-        <Route path="/allbookingsroom" element={<AllBookings />} />
-        <Route path="/cancelbook/:id" element={<CancelBooking />} />
-        <Route
-          path="/updateRoomsByIDcus/:id/:fromdate/:todate"
-          element={<Booking />}
-        />
-        <Route path="/comments-section" element={<CommentsSection />} />
-        <Route
-          path="/comments-section/create/:roomID"
-          element={<AddComment />}
-        />
-        <Route
-          path="/comments-section/edit/:roomID/:id"
-          element={<EditComment />}
-        />
-        <Route path="/allusers" element={<AllUsers />} />
       </Routes>
       <br />
       <Footer />
